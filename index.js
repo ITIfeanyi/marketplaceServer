2bd453c2ff48e16538ec9b06e9528fa5c1659b14
ITIfeanyi <ifeanyithankgodigweh@gmail.com>
2021-03-24 14:38:18 -0700

    add cors

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ require("./modals/db");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Method", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Options");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 //routes
 const homepageRoute = require("./Routes/index");
 const productRoute = require("./Routes/productRoutes");
