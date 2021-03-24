fbe998b56bc56738b167efd5fb197662c7e8eb8c
ITIfeanyi <ifeanyithankgodigweh@gmail.com>
2021-03-24 07:40:39 -0700

    added field reset after submit and uploaded product to the db

--- Routes/index.js
+++ Routes/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../modals/ProductSchema");
 
 router.get("/", (req, res) => {
   res.sendFile("index.html");
 });
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { title, price, category, imageURL, description } = req.body;
-  console.log(req.body);
+  try {
+    const newProduct = new Product({
+      title,
+      price,
+      category,
+      imageURL,
+      description,
+    });
+    await newProduct.save();
+    res.status(200).json({ message: "Product uploaded successfully" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
